f4016308a33adf337c6224018a9dffb833cda258
Daniel <krivelevdaniil@gmail.com>
2021-09-23 17:12:58 +0700

    Basic project setup.
    1. Installed all pods needed
    2. Configured SwiftLint
    3. Added App Icon
    4. Configured LaunchScreen
    5. Structured Folders and created basic files for following MVVM-C architecural pattern

---
+++ KODE-Weather/Scenes/AppCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  AppCoordinator.swift
+//  KODE-Weather
+//
+//  Created by Developer on 23.09.2021.
+//
+
+import UIKit
+
+final class AppCoordinator {
+    init(window: UIWindow) {
+        // TODO: - Create window property and set it to the received
+    }
+    
+    func start() {
+        // TODO: - setup start func (setup and start Scene)
+    }
+}
